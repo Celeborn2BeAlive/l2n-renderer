e5d99d0cbfd897d3ee9c562d399bf41f05fba448
Laurent Noel <laurent.noel.c2ba@gmail.com>
2016-04-29 14:18:49 +0200

    Add utility functions for loading and compiling shaders.

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,77 @@
 #include <c2ba/glutils/GLProgram.hpp>
 
 #include <filesystem>
+#include <unordered_map>
+
+namespace fs = std::experimental::filesystem;
+
+namespace std {
+template <> struct hash<fs::path>
+{
+    size_t operator()(const fs::path& x) const
+    {
+        using StringType = fs::path::string_type;
+        return hash<StringType>()(x.c_str());
+    }
+};
+}
 
 namespace l2n
 {
 
-namespace fs = std::experimental::filesystem;
+// Use .at() to access elements since GLShader has no default constructor
+using ShaderLibrary = std::unordered_map<fs::path, c2ba::GLShader>;
+
+c2ba::GLShader loadShader(const fs::path& shaderPath)
+{
+    using StringType = fs::path;
+    static auto extToShaderType = std::unordered_map<StringType, std::pair<GLenum, std::string>>({
+        { ".vs", { GL_VERTEX_SHADER, "vertex" } },
+        { ".fs", { GL_FRAGMENT_SHADER, "fragment" } }
+    });
+
+    auto ext = shaderPath.stem().extension();
+    auto it = extToShaderType.find(ext);
+    if (it == end(extToShaderType)) {
+        std::cerr << "Unrecognized shader extension " << ext << std::endl;
+        throw std::runtime_error("Unrecognized shader extension " + ext.string());
+    }
+
+    std::clog << "Compiling " << (*it).second.second << " shader " << shaderPath << "\n";
+
+    c2ba::GLShader shader{ (*it).second.first };
+    shader.setSource(c2ba::loadShaderSource(shaderPath));
+    shader.compile();
+    if (!shader.getCompileStatus()) {
+        std::cerr << "Shader compilation error:" << shader.getInfoLog() << std::endl;
+        throw std::runtime_error("Shader compilation error:" + shader.getInfoLog());
+    }
+    return shader;
+}
+
+void loadShaders(const fs::path& directoryPath, ShaderLibrary& shaderLibrary)
+{
+    for (auto& entry : fs::recursive_directory_iterator(directoryPath)) {
+        auto path = entry.path();
+        if (fs::is_regular_file(path) && path.extension() == ".glsl") {
+            shaderLibrary.emplace(std::make_pair(path, loadShader(path)));
+        }
+    }
+}
+
+c2ba::GLProgram compileProgram(const ShaderLibrary& shaderLibrary, std::vector<fs::path> shaderPaths)
+{
+    c2ba::GLProgram program;
+    for (const auto& path : shaderPaths) {
+        program.attachShader(shaderLibrary.at(path));
+    }
+    program.link();
+    if (!program.getLinkStatus()) {
+        std::cerr << "Program link error:" << program.getInfoLog() << std::endl;
+        throw std::runtime_error("Program link error:" + program.getInfoLog());
+    }
+    return program;
+}
 
 int main(int argc, char** argv)
 {
@@ -58,30 +124,11 @@ int main(int argc, char** argv)
     vao.enableVertexAttrib(1);
     vao.vertexAttribOffset(vbo.glId(), 1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 2 * sizeof(GLfloat));
 
-    c2ba::GLProgram program;
+    ShaderLibrary shaderLibrary;
+    auto shadersRoot = appDir / "glsl";
+    loadShaders(shadersRoot, shaderLibrary);
 
-    {
-        c2ba::GLShader vertexShader{ GL_VERTEX_SHADER };
-        vertexShader.setSource(c2ba::loadShaderSource(appDir / "glsl/triangle.vs.glsl"));
-        vertexShader.compile();
-        if (!vertexShader.getCompileStatus()) {
-            std::cerr << "Vertex Shader error:" << vertexShader.getInfoLog() << std::endl;
-        }
-
-        c2ba::GLShader fragmentShader{ GL_FRAGMENT_SHADER };
-        fragmentShader.setSource(c2ba::loadShaderSource(appDir / "glsl/triangle.fs.glsl"));
-        fragmentShader.compile();
-        if (!fragmentShader.getCompileStatus()) {
-            std::cerr << "Fragment Shader error:" << fragmentShader.getInfoLog() << std::endl;
-        }
-
-        program.attachShader(vertexShader);
-        program.attachShader(fragmentShader);
-        program.link();
-        if (!program.getLinkStatus()) {
-            std::cerr << "Program link error:" << program.getInfoLog() << std::endl;
-        }
-    }
+    auto program = compileProgram(shaderLibrary, { shadersRoot / "triangle.vs.glsl", shadersRoot / "triangle.fs.glsl" });
 
     program.use();
 
